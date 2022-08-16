b68cd3b1b943c5863ad55604eb5e21ddde64ca0e
André Rodrigues <andregrgr@hotmail.com>
2022-08-16 20:07:26 -0300

    feat: changes to the home page with creating cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import CartGenerator from './components/CardGernerator';
-import Cards from './components/Cards';
 import './styles/css/App.css'
 
 
@@ -9,8 +8,7 @@ function App() {
       <div className="intro">
         <h1 className="title"> Lista de afazeres</h1>
         <h3 className="subtitle">Coloque aqui as suas tarefas do dia para ter um melhor controle</h3>
-        <CartGenerator />
-        <Cards />        
+        <CartGenerator />   
       </div>
     </div>
   );
--- src/components/CardGernerator/index.jsx
+++ src/components/CardGernerator/index.jsx
@@ -1,16 +1,35 @@
+import { useState, useRef,useEffect } from 'react';
 import '../../styles/css/CardGenerator.css';
+import Cards from '../Cards';
+
 
 
 const CartGenerator = () => {
+    const [cards, setCards] = useState([]);
+    const inputRef = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('clicou');
+        const text = inputRef.current.value;
+        setCards([...cards, text]);
+        inputRef.current.value = ``;
+    }
+
+    useEffect(() => {
+        console.log(cards);
+    }, [cards])
+
     return (
         <div className="card-generator">
-            <form >
+            <form onSubmit={handleSubmit}>
                 <label for="task">Coloque a sua tarefa aqui</label>
-                <input type="text"name="task" />
+                <input type="text"name="task" ref={inputRef} />
                 <div className="card-generator_options">
-                    <button>Criar</button>
+                    <button type='Submit'>Criar</button>
                 </div>
             </form>
+            <Cards info={cards}/>
         </div>
     )
 }
